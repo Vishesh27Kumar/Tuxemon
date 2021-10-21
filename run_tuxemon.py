abb1ee8910bea5419dd25394b5ce569cb54c116a
Vishesh <45128646+Vishesh27Kumar@users.noreply.github.com>
2021-10-21 18:29:46 +0530

    Update run_tuxemon.py

--- run_tuxemon.py
+++ run_tuxemon.py
@@ -1,51 +1,129 @@
-14, William Edwards <shadowapex@gmail.com>,
-#                     Benjamin Bean <superman2k5@gmail.com>
-#
-# This file is part of Tuxemon.
-#
-# Tuxemon is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Tuxemon is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Tuxemon.  If not, see <http://www.gnu.org/licenses/>.
-#
-# Contributor(s):
-#
-# William Edwards <shadowapex@gmail.com>
-# Leif Theden <leif.theden@gmail.com
-#
-#
-from argparse import ArgumentParser
+<?php
 
-if __name__ == '__main__':
-    from tuxemon.core import prepare, main
-    config = prepare.CONFIG
+namespace App;
 
-    parser = ArgumentParser()
-    parser.add_argument('-m', '--mod', dest='mod', metavar='mymod', type=str, nargs='?',
-                        default=None, help='The mod directory used in the mods directory')
-    parser.add_argument('-l', '--load', dest='slot', metavar='1,2,3', type=int, nargs='?',
-                        default=None, help='The index of the save file to load')
-    parser.add_argument('-s', '--starting-map', dest='starting_map', metavar='map.tmx', type=str, nargs='?',
-                        default=None, help='The starting map')
-    parser.add_argument('-t', '--test-map', dest='test_map', type=str, nargs='?',
-                        default=None, help='Skip title screen and load map directly')
-    args = parser.parse_args()
+use Backpack\CRUD\CrudTrait;
+use Spatie\Permission\Traits\HasRoles;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Backpack\Base\app\Notifications\ResetPasswordNotification as ResetPasswordNotification;
 
-    if args.mod:
-        config.mods.insert(0, args.mod)
-    if args.starting_map:
-        config.starting_map = args.starting_map
-    if args.test_map:
-        config.starting_map = args.test_map
-        config.skip_titlescreen = True
-        config.splash = False
+class User extends Authenticatable
+{
+    use Notifiable, HasRoles, CrudTrait;
 
-    main.main(load_slot=args.slot)
+    /*
+    |--------------------------------------------------------------------------
+    | GLOBAL VARIABLES
+    |--------------------------------------------------------------------------
+    */
+    protected $table = 'users';
+    // protected $primaryKey = 'id';
+    // public $timestamps = false;
+    // protected $guarded = ['id'];
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'salutation',
+        'birthday',
+        'gender',
+        'active',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    public $notificationVars = [
+        'userSalutation',
+        'userName',
+        'userEmail',
+        'age',
+    ];
+
+    /*
+    |--------------------------------------------------------------------------
+    | NOTIFICATIONS VARIABLES
+    |--------------------------------------------------------------------------
+    */
+    public function notificationVariables()
+    {
+        return [
+            'userSalutation' => $this->user->salutation,
+            'userName'       => $this->user->name,
+            'userEmail'      => $this->user->email,
+            'age'            => $this->age(),
+        ];
+    }
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | FUNCTIONS
+    |--------------------------------------------------------------------------
+    */
+    public function sendPasswordResetNotification($token)
+    {
+        $this->notify(new ResetPasswordNotification($token));
+    }
+
+    public function age()
+    {
+        if ($this->birthday) {
+            return \Carbon\Carbon::createFromFormat('d-m-Y', $this->birthday)->age;
+        }
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | RELATIONS
+    |--------------------------------------------------------------------------
+    */
+    public function addresses()
+    {
+        return $this->hasMany('App\Models\Address');
+    }
+
+    public function companies()
+    {
+        return $this->hasMany('App\Models\Company');
+    }
+
+    public function orders()
+    {
+        return $this->hasMany('App\Models\Order');
+    }
+
+    public function cartRules()
+    {
+        return $this->belongsToMany('App\Models\CartRule');
+    }
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | SCOPES
+    |--------------------------------------------------------------------------
+    */
+
+    /*
+    |--------------------------------------------------------------------------
+    | ACCESORS
+    |--------------------------------------------------------------------------
+    */
+
+    /*
+    |--------------------------------------------------------------------------
+    | MUTATORS
+    |--------------------------------------------------------------------------
+    */
+    public function getBirthdayAttribute($value)
+    {
+        if ($value) {
+            return \Carbon\Carbon::createFromFormat('Y-m-d', $value)->format('d-m-Y');
+        }
+    }
+
+}
