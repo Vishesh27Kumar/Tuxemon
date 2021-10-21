6ca7fb6a01e53e3d5634e101a0034fe280eac475
Vishesh <45128646+Vishesh27Kumar@users.noreply.github.com>
2021-10-21 18:31:08 +0530

    Update run_tuxemon.py

--- run_tuxemon.py
+++ run_tuxemon.py
@@ -1,129 +1,14 @@
-<?php
-
-namespace App;
-
-use Backpack\CRUD\CrudTrait;
-use Spatie\Permission\Traits\HasRoles;
-use Illuminate\Notifications\Notifiable;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Backpack\Base\app\Notifications\ResetPasswordNotification as ResetPasswordNotification;
-
-class User extends Authenticatable
-{
-    use Notifiable, HasRoles, CrudTrait;
-
-    /*
-    |--------------------------------------------------------------------------
-    | GLOBAL VARIABLES
-    |--------------------------------------------------------------------------
-    */
-    protected $table = 'users';
-    // protected $primaryKey = 'id';
-    // public $timestamps = false;
-    // protected $guarded = ['id'];
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-        'salutation',
-        'birthday',
-        'gender',
-        'active',
-    ];
-
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    public $notificationVars = [
-        'userSalutation',
-        'userName',
-        'userEmail',
-        'age',
-    ];
-
-    /*
-    |--------------------------------------------------------------------------
-    | NOTIFICATIONS VARIABLES
-    |--------------------------------------------------------------------------
-    */
-    public function notificationVariables()
-    {
-        return [
-            'userSalutation' => $this->user->salutation,
-            'userName'       => $this->user->name,
-            'userEmail'      => $this->user->email,
-            'age'            => $this->age(),
-        ];
-    }
+dateISO: "Please enter a valid date ( ISO ).",
+		number: "Please enter a valid number.",
+		digits: "Please enter only digits.",
+		creditcard: "Please enter a valid credit card number.",
+		equalTo: "Please enter the same value again.",
+		maxlength: $.validator.format( "Please enter no more than {0} characters." ),
+		minlength: $.validator.format( "Please enter at least {0} characters." ),
 
 
-    /*
-    |--------------------------------------------------------------------------
-    | FUNCTIONS
-    |--------------------------------------------------------------------------
-    */
-    public function sendPasswordResetNotification($token)
-    {
-        $this->notify(new ResetPasswordNotification($token));
-    }
-
-    public function age()
-    {
-        if ($this->birthday) {
-            return \Carbon\Carbon::createFromFormat('d-m-Y', $this->birthday)->age;
-        }
-    }
-
-    /*
-    |--------------------------------------------------------------------------
-    | RELATIONS
-    |--------------------------------------------------------------------------
-    */
-    public function addresses()
-    {
-        return $this->hasMany('App\Models\Address');
-    }
-
-    public function companies()
-    {
-        return $this->hasMany('App\Models\Company');
-    }
-
-    public function orders()
-    {
-        return $this->hasMany('App\Models\Order');
-    }
-
-    public function cartRules()
-    {
-        return $this->belongsToMany('App\Models\CartRule');
-    }
-
-
-    /*
-    |--------------------------------------------------------------------------
-    | SCOPES
-    |--------------------------------------------------------------------------
-    */
-
-    /*
-    |--------------------------------------------------------------------------
-    | ACCESORS
-    |--------------------------------------------------------------------------
-    */
-
-    /*
-    |--------------------------------------------------------------------------
-    | MUTATORS
-    |--------------------------------------------------------------------------
-    */
-    public function getBirthdayAttribute($value)
-    {
-        if ($value) {
-            return \Carbon\Carbon::createFromFormat('Y-m-d', $value)->format('d-m-Y');
-        }
-    }
-
-}
+adapters.addSingleVal("regex", "pattern");
+    adapters.addBool("creditcard").addBool("date").addBool("digits").addBool("email").addBool("number").addBool("url");
+    adapters.addMinMax("length", "minlength", "maxlength", "rangelength").addMinMax("range", "min", "max", "range");
+    adapters.addMinMax("minlength", "minlength").addMinMax("maxlength", "minlength", "maxlength");
+    adapters.add("equalto", ["other"], function 
